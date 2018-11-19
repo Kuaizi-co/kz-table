7bdbc322c2a032e93678ede2cceb5c76be679883
邵金洪 <shaojinhong@kuaizi.co>
2018-11-19 17:55:08 +0800

    fix:es6 to es5

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,10 @@
 module.exports = {
   baseUrl: process.env.NODE_ENV === 'production'
     ? '/kz-table/'
-    : '/'
+    : '/',
+  configureWebpack: {
+    externals: {
+      'element-ui': 'element-ui'
+    }
+  }
 }
