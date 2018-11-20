f9fc0620140b63a2045262eee2da93cdea53f209
kz-fe <shaojinhong@kuaizi.co>
2018-11-20 10:07:41 +0800

    fix:deploy

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 const externals = process.env.NODE_ENV === 'production' ? { 'element-ui': 'element-ui'} : {}
-const outputDir = process.env.NODE_ENV === 'publish' ? 'kz-table' : 'dist'
+const outputDir = process.env.NODE_ENV === 'publish' ? 'docs' : 'dist'
+console.log(process.env.NODE_ENV, process.env.NODE_ENV === 'publish', outputDir)
 
 module.exports = {
   baseUrl: process.env.NODE_ENV === 'development' ? '/' :'/kz-table/',
